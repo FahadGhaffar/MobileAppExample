e9e6d72b4f545af88591e0414191f0d700cd2c6f
Fahad Ghaffar <fahad.ghaffar@lslogics.local>
2025-01-10 14:30:30 +0500

    new update

--- src/redux/myStore.js
+++ src/redux/myStore.js
@@ -1,1 +1,8 @@
-console.log("store");
+import { configureStore } from "@reduxjs/toolkit";
+import listReuces from './listSlices'
+const myStore  = configureStore({
+    reducer:{
+        name:listReuces,
+    }
+})
+export default myStore
